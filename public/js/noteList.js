2723214450ddeca4361c031eb97d7d50fb0f88dd
arthur <arthurfelipe5567@gmail.com>
2023-08-16 18:03:09 -0300

    feat: add some style to the main page

---
+++ public/js/noteList.js
@@ -0,0 +1,58 @@
+function addNoteRemove(note) {
+  const deleteButton = note.querySelector('.delete')
+
+  deleteButton.addEventListener('click', async () => {
+    /* const noteId = note.id.split('-')[1]
+
+    await fetch(`/notes/${noteId}`, {
+      method: 'DELETE'
+    }).then(res => res.ok).then(ok => {
+      if (ok) note.remove();
+      else alert('Erro ao deletar nota')
+    }) */
+
+    note.remove()
+  })
+}
+
+function generateView(noteData) {
+  const note = `
+  <div class="note" id="note-${noteData.id}">
+          <div class="noteTitle" style="user-select: none">Título da nota</div>
+          <div class="noteContent" style="user-select: none">
+            ${noteData.content}
+          </div>
+          <div class="noteOptions">
+            <div class="delete" style="user-select: none">
+              <iconify-icon
+                icon="ic:baseline-delete"
+                style="font-size: 1.51rem"
+              ></iconify-icon>
+            </div>
+          </div>
+        </div>
+  `
+
+  return note
+}
+
+function insertNote(noteData) {
+  const noteList = document.body.querySelector('#root')
+  const note = generateView(noteData)
+
+  noteList.insertAdjacentHTML('afterbegin', note)
+  const currentNote = noteList.querySelector(`#note-${noteData.id}`)
+
+  addNoteRemove(currentNote)
+}
+
+async function showNotes() {
+  const notes = await fetch('/notes/data').then(res => res.json())
+
+  notes.forEach(note => {
+    console.log(note)
+    insertNote(note)
+  })
+}
+
+showNotes()
