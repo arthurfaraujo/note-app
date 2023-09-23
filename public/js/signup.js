4770e1be5a9acd7af0bf277a1eaccebe62173633
arthur <arthurfelipe5567@gmail.com>
2023-09-23 16:11:04 -0300

    feat: add front validation
    only to signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,30 +1,56 @@
 const form = document.body.querySelector('form')
 
+function validateInput(inputField) {
+  const input = inputField.children[1]
+  const validationDiv = inputField.children[2]
+
+  if (input.checkValidity()) {
+    input.classList.add('valid')
+    input.classList.remove('invalid')
+    validationDiv.style.display = 'none'
+  } else {
+    input.classList.add('invalid')
+    input.classList.remove('valid')
+    validationDiv.style.display = 'block'
+  }
+}
+
 form.addEventListener('submit', async e => {
   e.preventDefault()
-  const formData = Object.fromEntries(new FormData(form))
 
-  const url = '/user/signup'
+  if (form.checkValidity()) {
+    const formData = Object.fromEntries(new FormData(form))
 
-  const reqConfig = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(formData)
-  }
+    const url = '/user/signup'
 
-  const res = await fetch(url, reqConfig).then(res => res.json())
+    const reqConfig = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(formData)
+    }
 
-  if (res.created) {
-    window.location.href = '/user/signin'
+    const res = await fetch(url, reqConfig).then(res => res.json())
+
+    if (res.created) {
+      window.location.href = '/user/signin'
+    } else {
+      for (const error of res.errors) {
+        const capitalizedLocation =
+          error.location.charAt(0).toUpperCase() + error.location.slice(1)
+
+        const string = error.message.replace('String', capitalizedLocation)
+        alert(string)
+      }
+    }
   } else {
-    for (const error of res.errors) {
-      const capitalizedLocation = error.location.charAt(0).toUpperCase() + error.location.slice(1)
+    const inputFields = form.querySelectorAll('.inputField')
 
-      const string = error.message.replace(
-        'String',
-        capitalizedLocation
-      )
-      alert(string)
+    for (let i = 0; i < inputFields.length; i++) {
+      validateInput(inputFields.item(i))
+
+      const input = inputFields.item(i).children[1]
+
+      input.addEventListener('input', () => validateInput(inputFields.item(i)))
     }
   }
 })
