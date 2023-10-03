6d429edcc33c556ec46c39f9e72a8d4b6fd89928
arthur <arthurfelipe5567@gmail.com>
2023-10-03 13:47:15 -0300

    fix: create interface where request have token

--- src/controllers/noteController.ts
+++ src/controllers/noteController.ts
@@ -1,35 +1,42 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response } from 'express'
 import Note from '../models/Note'
+import { ITokenRequest } from '../interfaces/authInterfaces'
 
 export async function noteListGet(req: Request, res: Response) {
   res.render('noteList')
 }
 
-async function noteListGetData(req: Request, res: Response) {
-  const { token } = req.body
+async function noteListGetData(req: ITokenRequest, res: Response) {
+  const { nickname } = req.token as {
+    nickname: string
+    iat: number
+    exp: number
+  }
 
-  const notes = await Note.readAllByUser(token.nickname)
+  const notes = await Note.readAllByUser(nickname)
 
   return res.json(notes)
 }
 
-async function noteCreatePost(req: Request, res: Response) {
+async function noteCreatePost(req: ITokenRequest, res: Response) {
   const noteData = req.body
-  const { nickname } = req.body.token
-
-  delete noteData.token
+  const { nickname } = req.token as {
+    nickname: string
+    iat: number
+    exp: number
+  }
 
   const result = await Note.create({
     ...noteData,
     userNickname: nickname
   })
 
-  return res.json({ created: result.created, id: result.id  })
+  return res.json({ created: result.created, id: result.id })
 }
 
 async function noteDelete(req: Request, res: Response) {
-  const noteId = req.params.id
-  const note = await Note.deleteById(Number(noteId))
+  const noteId = Number(req.params.id)
+  const note = await Note.deleteById(noteId)
 
   res.json(note)
 }
