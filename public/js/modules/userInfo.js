3ccb2790c97b67debe5244286a6fdd5dbba13b2d
arthur <arthurfelipe5567@gmail.com>
2023-12-08 17:01:43 -0300

    feat: add photo source changing

--- public/js/modules/userInfo.js
+++ public/js/modules/userInfo.js
@@ -1,6 +1,7 @@
 import { getToken } from './auth.js'
 
 const userName = document.body.querySelector('.userName')
+const userPhoto = document.body.querySelector('.userPhoto')
 
 async function getUserInfo() {
   const reqConfig = {
@@ -14,6 +15,8 @@ async function getUserInfo() {
 
   const userInfo = await fetch(url, reqConfig).then(res => res.json())
 
+  console.log(userInfo)
+
   return userInfo
 }
 
@@ -21,6 +24,10 @@ async function renderUserInfo() {
   const user = await getUserInfo()
 
   userName.textContent = user.nickname
+
+  if (user.photo) {
+    userPhoto.src = user.photo
+  }
 }
 
 export default renderUserInfo
